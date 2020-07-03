deb2faf073dc581cbb295db3e7778e7614dc2d9e
Marek Bejda <marek.bejda@ibm.com>
2020-07-03 07:16:05 -0400

    login

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,10 +5,15 @@ invocation_error=0
 
 # - BXIAM: IBM Cloud API key
 if [ -z ${APIKEY+x} ]; then echo 'Error: Environment variable BXIAM is undefined.'; invocation_error=1; fi
+# set optional Cloud Foundry variables if they are not set
+# - CF_API: IBM Cloud API endpoint (default to US-South region)
+if [ -z ${CF_API+x} ]; then export CF_API='https://api.ng.bluemix.net'; fi
+
+if [ ${invocation_error} -eq 1 ]; then echo 'Something went wrong, check for previous errors.'; exit 1; fi
 
 # login and set target
 ./Bluemix_CLI/bin/ibmcloud config --check-version false
-./Bluemix_CLI/bin/ibmcloud login --apikey $APIKEY
+./Bluemix_CLI/bin/ibmcloud login -a $CF_API --apikey $APIKEY
 ./Bluemix_CLI/bin/ibmcloud target --cf
 ./Bluemix_CLI/bin/ibmcloud plugin install cloud-functions -r Bluemix
 ./Bluemix_CLI/bin/ibmcloud fn action update smdg_code_lookup --kind python-jessie:3 ./endpoint/main.py
